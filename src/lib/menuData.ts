4097343b67168d28ba2c28a9ba89c2ce1217c070
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2025-01-02 22:02:18 +0600

    solved the deployment errros

--- src/lib/menuData.ts
+++ src/lib/menuData.ts
@@ -1,147 +1,147 @@
-import { Menu } from "@/types/menu";
+// import { Menu } from "@/types/menu";
 
-export const MENU_DATA: Menu = [
-  {
-    id: 1,
-    title: "Appetizers",
-    itemCount: 3,
-    likes: 120,
-    imageUrl:
-      "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-    items: [
-      {
-        id: 1,
-        name: "Bruschetta",
-        calories: "120 ",
-        price: 8.99,
-        demoPrice: 11.99,
-        discountPercentage: 20,
-        likes: "95%",
-        image:
-          "https://images.unsplash.com/photo-1572695157366-5e585ab2b69f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 2,
-        name: "Mozzarella Sticks",
-        calories: "320 ",
-        price: 7.99,
-        demoPrice: 8.99,
-        discountPercentage: 10,
-        likes: "88%",
-        image:
-          "https://images.unsplash.com/photo-1531749668029-2db88e4276c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 3,
-        name: "Spinach Artichoke Dip",
-        calories: "250",
-        price: 9.99,
-        demoPrice: 10.99,
-        discountPercentage: 5,
-        likes: "92%",
-        image:
-          "https://images.unsplash.com/photo-1576506295286-5cda18df43e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "Main Courses",
-    itemCount: 4,
-    likes: 250,
-    imageUrl:
-      "https://images.unsplash.com/photo-1556910103-1c02745aae4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-    items: [
-      {
-        id: 4,
-        name: "Grilled Salmon",
-        calories: "400",
-        price: 18.99,
-        demoPrice: 20.99,
+// export const MENU_DATA: Menu = [
+//   {
+//     id: 1,
+//     title: "Appetizers",
+//     itemCount: 3,
+//     likes: 120,
+//     imageUrl:
+//       "https://images.unsplash.com/photo-1626645738196-c2a7c87a8f58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//     items: [
+//       {
+//         id: 1,
+//         name: "Bruschetta",
+//         calories: "120 ",
+//         price: 8.99,
+//         demoPrice: 11.99,
+//         discountPercentage: 20,
+//         likes: "95%",
+//         image:
+//           "https://images.unsplash.com/photo-1572695157366-5e585ab2b69f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 2,
+//         name: "Mozzarella Sticks",
+//         calories: "320 ",
+//         price: 7.99,
+//         demoPrice: 8.99,
+//         discountPercentage: 10,
+//         likes: "88%",
+//         image:
+//           "https://images.unsplash.com/photo-1531749668029-2db88e4276c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 3,
+//         name: "Spinach Artichoke Dip",
+//         calories: "250",
+//         price: 9.99,
+//         demoPrice: 10.99,
+//         discountPercentage: 5,
+//         likes: "92%",
+//         image:
+//           "https://images.unsplash.com/photo-1576506295286-5cda18df43e7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     title: "Main Courses",
+//     itemCount: 4,
+//     likes: 250,
+//     imageUrl:
+//       "https://images.unsplash.com/photo-1556910103-1c02745aae4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//     items: [
+//       {
+//         id: 4,
+//         name: "Grilled Salmon",
+//         calories: "400",
+//         price: 18.99,
+//         demoPrice: 20.99,
 
-        discountPercentage: 10,
-        likes: "96%",
-        image:
-          "https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 5,
-        name: "Chicken Parmesan",
-        calories: "650",
-        price: 15.99,
-        demoPrice: 16,
-        discountPercentage: 15,
-        likes: "94%",
-        image:
-          "https://images.unsplash.com/photo-1632778149955-e80f8ceca2e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 6,
-        name: "Vegetable Stir Fry",
-        calories: "300",
-        price: 13.99,
-        demoPrice: 14.99,
+//         discountPercentage: 10,
+//         likes: "96%",
+//         image:
+//           "https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 5,
+//         name: "Chicken Parmesan",
+//         calories: "650",
+//         price: 15.99,
+//         demoPrice: 16,
+//         discountPercentage: 15,
+//         likes: "94%",
+//         image:
+//           "https://images.unsplash.com/photo-1632778149955-e80f8ceca2e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 6,
+//         name: "Vegetable Stir Fry",
+//         calories: "300",
+//         price: 13.99,
+//         demoPrice: 14.99,
 
-        discountPercentage: 5,
-        likes: "89%",
-        image:
-          "https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 7,
-        name: "Beef Tenderloin",
-        calories: "500",
-        price: 24.99,
-        demoPrice: 25.99,
-        discountPercentage: 20,
-        likes: "97%",
-        image:
-          "https://images.unsplash.com/photo-1588168333986-5078d3ae3976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: "Desserts",
-    itemCount: 3,
-    likes: 180,
-    imageUrl:
-      "https://images.unsplash.com/photo-1488477181946-6428a0291777?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-    items: [
-      {
-        id: 8,
-        name: "Chocolate Lava Cake",
-        calories: "400",
-        price: 7.99,
-        demoPrice: 8.99,
-        discountPercentage: 15,
-        likes: "98%",
-        image:
-          "https://images.unsplash.com/photo-1624353365286-3f8d62daad51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 9,
-        name: "New York Cheesecake",
-        calories: "350",
-        price: 6.99,
-        demoPrice: 7.99,
-        discountPercentage: 10,
-        likes: "95%",
-        image:
-          "https://images.unsplash.com/photo-1524351199678-941a58a3df50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-      {
-        id: 10,
-        name: "Fruit Tart",
-        calories: "280",
-        price: 5.99,
-        demoPrice: 6.99,
+//         discountPercentage: 5,
+//         likes: "89%",
+//         image:
+//           "https://images.unsplash.com/photo-1512058564366-18510be2db19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 7,
+//         name: "Beef Tenderloin",
+//         calories: "500",
+//         price: 24.99,
+//         demoPrice: 25.99,
+//         discountPercentage: 20,
+//         likes: "97%",
+//         image:
+//           "https://images.unsplash.com/photo-1588168333986-5078d3ae3976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//     ],
+//   },
+//   {
+//     id: 3,
+//     title: "Desserts",
+//     itemCount: 3,
+//     likes: 180,
+//     imageUrl:
+//       "https://images.unsplash.com/photo-1488477181946-6428a0291777?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//     items: [
+//       {
+//         id: 8,
+//         name: "Chocolate Lava Cake",
+//         calories: "400",
+//         price: 7.99,
+//         demoPrice: 8.99,
+//         discountPercentage: 15,
+//         likes: "98%",
+//         image:
+//           "https://images.unsplash.com/photo-1624353365286-3f8d62daad51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 9,
+//         name: "New York Cheesecake",
+//         calories: "350",
+//         price: 6.99,
+//         demoPrice: 7.99,
+//         discountPercentage: 10,
+//         likes: "95%",
+//         image:
+//           "https://images.unsplash.com/photo-1524351199678-941a58a3df50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//       {
+//         id: 10,
+//         name: "Fruit Tart",
+//         calories: "280",
+//         price: 5.99,
+//         demoPrice: 6.99,
 
-        discountPercentage: 5,
-        likes: "91%",
-        image:
-          "https://images.unsplash.com/photo-1488477304112-4944851de03d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
-      },
-    ],
-  },
-];
+//         discountPercentage: 5,
+//         likes: "91%",
+//         image:
+//           "https://images.unsplash.com/photo-1488477304112-4944851de03d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+//       },
+//     ],
+//   },
+// ];
