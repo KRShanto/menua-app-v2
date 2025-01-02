49bdce06f1ace53f5b90b9e6b5c97fd3196b2ba4
KR Shanto <krshanto2005@gmail.com>
2025-01-02 13:34:02 +0600

    feat: fetch discount items.

--- src/components/DiscountSection.tsx
+++ src/components/DiscountSection.tsx
@@ -1,79 +1,73 @@
 import { FaFire, FaTags } from "react-icons/fa";
 import { GoPlus } from "react-icons/go";
 import BottomDrawer from "./BottomDrawer";
-import { useState } from "react";
-import { MenuItem } from "@/types/menu";
+import { useState, useEffect } from "react";
+import { MenuItem } from "@/lib/firebase";
 import { useAddToCartStore } from "@/stores/useAddToCart";
+import {
+  db,
+  DISCOUNT_COLLECTION,
+  MENU_COLLECTION,
+  storage,
+} from "@/lib/firebase";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { getDownloadURL, ref } from "firebase/storage";
 
-const discountData = [
-  {
-    name: "This Week's Special",
-    discountRate: 25,
-    items: [
-      {
-        id: 1,
-        name: "Chicken of Cream Soup",
-        price: 100,
-        demoPrice: 120,
-        image:
-          "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        description: "A delicious soup made with chicken and cream.",
-        calories: "500",
-        likes: "15",
-      },
-      {
-        id: 2,
-        name: "Beef of Cream Soup",
-        price: 150,
-        demoPrice: 200,
-        image:
-          "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        description: "A delicious soup made with beef and cream.",
-        calories: "600",
-        likes: "8",
-      },
-      {
-        id: 3,
-        name: "Chicken nuggets",
-        price: 120,
-        demoPrice: 150,
-        image:
-          "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        description: "Delicious chicken nuggets.",
-        calories: "700",
-        likes: "10",
-      },
-      {
-        id: 4,
-        name: "Beef nuggets",
-        price: 130,
-        demoPrice: 160,
-        image:
-          "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        description: "Delicious beef nuggets.",
-        calories: "800",
-        likes: "12",
-      },
-      {
-        id: 5,
-        name: "French fries",
-        price: 100,
-        demoPrice: 120,
-        image:
-          "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        description: "Delicious french fries.",
-        calories: "900",
-        likes: "10",
-      },
-    ],
-  },
-];
+interface EnhancedMenuItem extends MenuItem {
+  image: string;
+  discountRate: number;
+}
 
 export default function DiscountSection() {
   const [selectedItem, setSelectedItem] = useState<MenuItem | null>(null);
   const [drawerOpen, setDrawerOpen] = useState(false);
   const { itemQuantity, increment, decrement, showToSlide } =
     useAddToCartStore();
+  const [discountData, setDiscountData] = useState<EnhancedMenuItem[]>([]);
+
+  useEffect(() => {
+    const fetchDiscounts = async () => {
+      const discountSnapshot = await getDocs(
+        collection(db, DISCOUNT_COLLECTION),
+      );
+      const discounts = discountSnapshot.docs.map((doc) => doc.data());
+
+      console.log("Discounts: ", discounts);
+
+      // Fetch all related menu items with discounts
+      const items = await Promise.all(
+        discounts.map(async (discount) => {
+          const itemSnapshot = await getDoc(
+            doc(db, MENU_COLLECTION, discount.itemId),
+          );
+          const menuItems = itemSnapshot?.data() as MenuItem;
+
+          console.log("Menu items: ", menuItems);
+
+          // download image
+          const storageRef = ref(storage, menuItems.imageURL);
+          const imageUrl = await getDownloadURL(storageRef);
+          menuItems.imageURL = imageUrl;
+
+          // add discount rate
+          const enhancedItem: EnhancedMenuItem = {
+            ...menuItems,
+            image: imageUrl,
+            discountRate: discount.rate,
+          };
+
+          return enhancedItem;
+        }),
+      );
+
+      // Flatten the items array
+      setDiscountData(items);
+    };
+
+    fetchDiscounts();
+  }, []);
+
+  console.log("Discount data: ", discountData);
 
   const handleItemClick = (item: MenuItem) => {
     setSelectedItem(item);
@@ -87,63 +81,61 @@ export default function DiscountSection() {
 
   return (
     <div>
-      {discountData.map((discount, index) => (
-        <div key={index} className="p-4">
-          {/* Discount Title */}
-          <h2 className="flex items-center gap-2 text-xl text-foregroundColor">
-            <FaFire />
-            {discount.name}
-          </h2>
+      <div className="p-4">
+        {/* Discount Title */}
+        <h2 className="flex items-center gap-2 text-xl text-foregroundColor">
+          <FaFire />
+          This Week's Special
+        </h2>
 
-          {/* Item List */}
-          <ul className="mt-2 flex gap-3 overflow-x-scroll">
-            {discount.items.map((item) => (
-              <li
-                key={item.name}
-                className="relative min-w-[16rem]"
-                onClick={() => handleItemClick(item)}
-              >
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="h-1/2 w-full rounded-tl-xl rounded-tr-xl"
-                />
-                <div className="h-1/2 rounded-bl-xl rounded-br-xl bg-[#1F1F20] p-4">
-                  <h3 className="text-lg text-foregroundColor">{item.name}</h3>
-                  <p className="-mb-16 mt-5 text-foregroundColor">
+        {/* Item List */}
+        <ul className="mt-2 flex gap-3 overflow-x-scroll">
+          {discountData.map((item: any) => (
+            <li
+              key={item.name}
+              className="relative min-w-[16rem]"
+              onClick={() => handleItemClick(item)}
+            >
+              <img
+                src={item.imageURL}
+                alt={item.name}
+                className="h-1/2 w-full rounded-tl-xl rounded-tr-xl"
+              />
+              <div className="h-1/2 rounded-bl-xl rounded-br-xl bg-[#1F1F20] p-4">
+                <h3 className="text-lg text-foregroundColor">{item.name}</h3>
+                <p className="-mb-16 mt-5 text-foregroundColor">
+                  SR {item.price * (1 - item.discountRate / 100)}
+                  <span className="ml-3 text-sm line-through opacity-60">
                     SR {item.price}
-                    <span className="ml-3 text-sm line-through opacity-60">
-                      SR {item.price * (1 - discount.discountRate / 100)}
-                    </span>
-                  </p>
-                </div>
+                  </span>
+                </p>
+              </div>
 
-                {/* Discount Tag */}
-                <div className="absolute left-4 top-4 flex items-center gap-2 rounded-full bg-foregroundColor px-2 py-1 text-black">
-                  <FaTags />
-                  {discount.discountRate}% Off
-                </div>
+              {/* Discount Tag */}
+              <div className="absolute left-4 top-4 flex items-center gap-2 rounded-full bg-foregroundColor px-2 py-1 text-black">
+                <FaTags />
+                {item.discountRate}% Off
+              </div>
 
-                {/* Add button */}
-                {itemQuantity === 0 || !showToSlide ? (
-                  <button
-                    className="absolute right-3 top-[40%] flex items-center gap-1 rounded-full bg-[#D87E27] px-4 py-1 text-black"
-                    // onClick={add}
-                  >
-                    Add <GoPlus />
-                  </button>
-                ) : (
-                  <div className="absolute right-3 top-[40%] flex items-center gap-1 rounded-full bg-[#D87E27] px-4 py-1 text-black">
-                    <button onClick={decrement}>-</button>
-                    <span>{itemQuantity}</span>
-                    <button onClick={increment}>+</button>
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+              {/* Add button */}
+              {itemQuantity === 0 || !showToSlide ? (
+                <button
+                  className="absolute right-3 top-[40%] flex items-center gap-1 rounded-full bg-[#D87E27] px-4 py-1 text-black"
+                  // onClick={add}
+                >
+                  Add <GoPlus />
+                </button>
+              ) : (
+                <div className="absolute right-3 top-[40%] flex items-center gap-1 rounded-full bg-[#D87E27] px-4 py-1 text-black">
+                  <button onClick={decrement}>-</button>
+                  <span>{itemQuantity}</span>
+                  <button onClick={increment}>+</button>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
       <BottomDrawer
         item={selectedItem}
         open={drawerOpen}
