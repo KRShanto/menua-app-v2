7f4e865df823f469df1ab3f65c74ee9874835fde
KR Shanto <krshanto2005@gmail.com>
2025-02-26 16:48:24 +0600

    feat: hide Footer component on order history page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,8 @@
 export default function Footer() {
+  const pathname = window.location.pathname;
+  if (pathname === "/orderhistory") {
+    return null;
+  }
+
   return <div className="h-16"></div>;
 }
